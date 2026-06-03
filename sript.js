1e5b9f65ade4bdc8d732f9dca51853b2a6745d10
Umar Abdul Rehman <umararjamali@gmail.com>
2026-06-03 21:38:20 +0500

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,108 @@
+const units = {
+    Length: {
+        Meters: 1,
+        Kilometers: 1000,
+        Centimeters: 0.01,
+        Feet: 0.3048,
+        Inches: 0.0254,
+        Miles: 1609.34
+    },
+
+    Weight: {
+        Kilograms: 1,
+        Grams: 0.001,
+        Pounds: 0.453592,
+        Ounces: 0.0283495
+    },
+
+    Storage: {
+        Bytes: 1,
+        Kilobytes: 1024,
+        Megabytes: 1048576,
+        Gigabytes: 1073741824
+    }
+};
+
+const category = document.getElementById("category");
+const fromUnit = document.getElementById("fromUnit");
+const toUnit = document.getElementById("toUnit");
+const inputValue = document.getElementById("inputValue");
+const resultText = document.getElementById("resultText");
+const convertBtn = document.getElementById("convertBtn");
+
+function loadUnits() {
+    fromUnit.innerHTML = "";
+    toUnit.innerHTML = "";
+
+    const selectedCategory = category.value;
+
+    if (selectedCategory === "Temperature") {
+        const tempUnits = ["Celsius", "Fahrenheit", "Kelvin"];
+
+        tempUnits.forEach(unit => {
+            fromUnit.innerHTML += `<option>${unit}</option>`;
+            toUnit.innerHTML += `<option>${unit}</option>`;
+        });
+
+        return;
+    }
+
+    for (let unit in units[selectedCategory]) {
+        fromUnit.innerHTML += `<option>${unit}</option>`;
+        toUnit.innerHTML += `<option>${unit}</option>`;
+    }
+}
+
+function convert() {
+    const value = Number(inputValue.value);
+
+    if (!value && value !== 0) {
+        resultText.textContent = "Enter a value";
+        return;
+    }
+
+    const categoryType = category.value;
+    const from = fromUnit.value;
+    const to = toUnit.value;
+
+    let result;
+
+    if (categoryType === "Temperature") {
+
+        if (from === to) {
+            result = value;
+        }
+        else if (from === "Celsius" && to === "Fahrenheit") {
+            result = (value * 9 / 5) + 32;
+        }
+        else if (from === "Celsius" && to === "Kelvin") {
+            result = value + 273.15;
+        }
+        else if (from === "Fahrenheit" && to === "Celsius") {
+            result = (value - 32) * 5 / 9;
+        }
+        else if (from === "Fahrenheit" && to === "Kelvin") {
+            result = ((value - 32) * 5 / 9) + 273.15;
+        }
+        else if (from === "Kelvin" && to === "Celsius") {
+            result = value - 273.15;
+        }
+        else if (from === "Kelvin" && to === "Fahrenheit") {
+            result = ((value - 273.15) * 9 / 5) + 32;
+        }
+
+    } else {
+
+        result =
+            value *
+            units[categoryType][from] /
+            units[categoryType][to];
+    }
+
+    resultText.textContent = result.toFixed(2);
+}
+
+loadUnits();
+
+category.addEventListener("change", loadUnits);
+convertBtn.addEventListener("click", convert);
